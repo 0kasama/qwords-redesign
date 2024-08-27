9ab27238dd2a878e0f237c74522a743c23d004bb
0kasama <okaajyad@gmail.com>
2024-08-27 16:24:17 +0700

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                bumblebee: {
+                    ...require("daisyui/src/theming/themes")["bumblebee"],
+                    primary: "#FF6D0B",
+                    secondary: "#FFF8F3",
+                    accent: "#D75600",
+                },
+            },
+        ],
+    },
+};
