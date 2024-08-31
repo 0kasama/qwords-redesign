1e1049dde7c6a3795788d9123fdf35b919b82e84
0kasama <okaajyad@gmail.com>
2024-08-31 21:36:52 +0700

    chore: create solution data

---
+++ app/Data/SolutionData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Data;
+
+class SolutionData
+{
+    public static function getSolution()
+    {
+        return [
+            [
+                'title' => 'Perusahaan',
+                'detail' => 'Wujudkan website perusahaan profesional dan berkelas dengan dukungan web hosting terbaik dan fitur keamanan ekstra.',
+                'url' => '/',
+                'img' => 'assets/graphics/organization-rafiki.png',
+            ],
+            [
+                'title' => 'UMKM dan Toko Online',
+                'detail' => 'Memberikan hosting terbaik untuk mendukung kebutuhan website pemasaran barang/jasa secara online.',
+                'url' => '/',
+                'img' => 'assets/graphics/umkm-amico.png',
+            ],
+            [
+                'title' => 'Organisasi dan Komunitas',
+                'detail' => 'Bikin website organisasi atau komunitas Anda dengan paket hosting unggulan yang ramah kantong.',
+                'url' => '/',
+                'img' => 'assets/graphics/community-pana.png',
+            ],
+            [
+                'title' => 'Sekolah',
+                'detail' => 'Infrastruktur web hosting terbaik untuk menunjang kegiatan e-learning. Didukung platform MOODLE khusus pembelajaran daring.',
+                'url' => '/',
+                'img' => 'assets/graphics/student-cuate.png',
+            ],
+            [
+                'title' => 'Developer',
+                'detail' => 'Tersedia paket hosting Indonesia dengan spesifikasi khusus yang dev-friendly. Ekstra NodeJS dan Git Version Control.',
+                'url' => '/',
+                'img' => 'assets/graphics/developer-amico.png',
+            ],
+            [
+                'title' => 'Blogger dan Personal',
+                'detail' => 'Memfasilitasi blogger dengan infrastruktur hosting Indonesia terbaik serta template website premium untuk tampil lebih profesional.',
+                'url' => '/',
+                'img' => 'assets/graphics/personal-cuate.png',
+            ],
+        ];
+    }
+}
