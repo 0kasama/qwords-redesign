fed7e2fa60e0025592d4b2d42b647be76d46c9d0
0kasama <okaajyad@gmail.com>
2024-09-02 01:17:56 +0700

    feat: create faq

---
+++ resources/views/components/faq.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex flex-col w-full justify-center items-center px-10 m-10 gap-5">
+        <h1 class="font-extrabold text-5xl text-center mb-5">Pertanyaan seputar <span class="text-primary">Layanan Web
+                Hosting</span> Qwords</h1>
+        <div class="flex flex-col lg:w-5/6 w-full gap-5 text-left text-pretty">
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Apa itu web hosting?</div>
+                <div class="flex flex-col collapse-content gap-5">
+                    <p>Hosting adalah tempat penyimpanan file dan data website seperti teks, video dan gambar website.
+                        File dan data tersebut di simpan di dalam server yang terkoneksi dengan internet sehingga data
+                        tersebut dapat diakses oleh siapa saja, kapan saja dan di mana saja.</p>
+                    <p>Penjelasan lebih lanjut dapat Anda baca pada <a href="https://qwords.com/blog/apa-itu-hosting/"
+                            class="link link-primary">artikel ini.</a></p>
+                </div>
+            </div>
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Apa saja jenis hosting di Qwords?</div>
+                <div class="flex flex-col collapse-content">
+                    <p class="mb-5">Untuk mendukung kesuksesan bisnis di era digital, Qwords menghadirkan beragam
+                        paket hosting Indonesia yang bisa dipilih sesuai kebutuhan.</p>
+                    <h1 class="text-3xl font-bold mb-2">1. Cloud Hosting Lite</h1>
+                    <p class="mb-5">Layanan cloud hosting berbasis panel DirectAdmin dengan performa tinggi, kapasitas
+                        penyimpanan besar, gratis domain, dan berbagai kelengkapan fitur lain hanya Rp3.000-an/GB.</p>
+                    <h1 class="text-3xl font-bold mb-2">2. Cloud Hosting Pro</h1>
+                    <p class="mb-5">Layanan cloud hosting berbasis cPanel dengan berbagai fitur dev-friendly dan cocok
+                        untuk bisnis profesional. Dilengkapi dengan fitur Boost Power untuk meningkatkan performa
+                        website hingga 200% saat dibutuhkan.</p>
+                    <h1 class="text-3xl font-bold mb-2">3. Cloud Hosting Enterprise</h1>
+                    <p class="mb-5">Layanan cloud hosting dengan resource CPU, RAM, & storage khusus satu pengguna dan
+                        tidak terbagi dengan pengguna lain. Performa website tetap stabil walaupun saat traffic tinggi.
+                    </p>
+                    <h1 class="text-3xl font-bold mb-2">4. Cloud Hosting WP</h1>
+                    <p class="mb-5">Layanan cloud hosting khusus WordPress menggunakan cPanel. Membangun website jadi
+                        lebih cepat dengan template premium melalui Elementor Pro gratis. Dilengkapi dengan gratis
+                        add-on sistem layanan payment gateway yang terintegrasi.</p>
+                    <h1 class="text-3xl font-bold mb-2">5. Unlimited Hosting</h1>
+                    <p class="mb-5">Layanan cloud hosting dengan resource tak terbatas. Spesifikasi penyimpanan,
+                        bandwidth, email, subdomain, dan database tidak dibatasi penggunaannya atau unlimited.</p>
+                    <h1 class="text-3xl font-bold mb-2">6. Flex WP</h1>
+                    <p>Layanan cloud hosting berbasis WordPress dengan menggunakan container. Anda bebas add-on CPU dan
+                        RAM sesuai kebutuhan. Rancangan kontainerisasi dan automasi Kubernetes memungkinkan website
+                        selalu terhindar dari gangguan di level server.</p>
+                </div>
+            </div>
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Apa kelebihan dari cloud hosting Qwords?</div>
+                <div class="flex flex-col collapse-content gap-5">
+                    <p>Cloud hosting Qwords sudah dipercaya oleh 60.000+ pelanggan hosting dan 100.000+ pelanggan domain
+                        dari dalam dan luar negeri. Anda akan mendapatkan jaminan kualitas dan keamanan, uptime 99.99%,
+                        dukungan teknis selama 24 jam setiap hari, dan berbagai pilihan cloud hosting dengan fitur
+                        lengkap yang bisa disesuaikan dengan kebutuhan.</p>
+                </div>
+            </div>
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Mengapa saya perlu web hosting?</div>
+                <div class="flex flex-col collapse-content gap-5">
+                    <p>Tanpa web hosting, Anda tidak bisa membuat website company profile, portal berita, blog pribadi,
+                        website toko online, atau jenis website lain yang bisa diakses secara online.</p>
+                    <p>Penyedia layanan web hosting seperti Qwords.com membantu memastikan data-data dan file website
+                        Anda tetap aman serta responsif ketika diakses oleh pengguna secara online.</p>
+                </div>
+            </div>
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Bagaimana cara membeli cloud hosting di Qwords?</div>
+                <div class="flex flex-col collapse-content gap-5">
+                    <p>Anda bisa membeli cloud hosting di Qwords dengan cara pilih paket cloud hosting yang terdapat di
+                        menu bar paling atas. Panduan cara membeli hosting di Qwords juga bisa Anda lihat <a
+                            href="https://qwords.com/blog/cara-membeli-hosting/" class="link link-primary">di sini</a>.
+                        Transaksi juga makin mudah dengan berbagai metode pembayaran yang tersedia.</p>
+                </div>
+            </div>
+            <div tabindex="0" class="collapse collapse-arrow border-base-300 bg-base-200 border">
+                <div class="collapse-title text-xl font-extrabold">Apakah saya bisa transfer hosting ke Qwords Cloud Web
+                    Hosting Indonesia?</div>
+                <div class="flex flex-col collapse-content gap-5">
+                    <p>Bisa. Caranya juga mudah. Anda tinggal melakukan pemesanan paket cloud hosting yang diinginkan
+                        dan klik “Ya” pada pilihan “Migrasi Hosting ke Qwords” di halaman portal order. Untuk detail
+                        panduan migrasi hosting ke Qwords, Anda bisa melihatnya <a
+                            href="https://www.qwords.com/migrasi-hosting/" class="link link-primary">di sini.</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
