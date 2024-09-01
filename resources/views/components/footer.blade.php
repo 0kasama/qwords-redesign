8c9e929e9d9f3c194050edbbb2848a27fd96c3b4
0kasama <okaajyad@gmail.com>
2024-09-02 01:18:35 +0700

    feat: create footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <footer class="footer bg-neutral text-white grid-cols-4 p-10">
+        <div class="flex flex-col gap-5">
+            <nav class="flex flex-col gap-5">
+                <img src="{{ asset('assets/qwords-bnw.webp') }}" width="150" alt="qwords logo" />
+                <h1 class="font-extrabold text-2xl">PT Qwords Company International</h1>
+                <p>Cloud Web Hosting Indonesia. Domain & hosting terbaik dengan akses cepat yang didukung layanan
+                    support 24/7</p>
+                <a
+                    href="https://www.bsigroup.com/en-ID/validate-bsi-issued-certificates/client-directory-certificate/IS%20776219"><img
+                        src="{{ asset('assets/footer/bsi.webp') }}" width="150" alt="bsi logo" /></a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Customer Care</h1>
+                <a class="link link-footer">Pembayaran</a>
+                <a class="link link-footer">Faktur Pajak</a>
+                <a class="link link-footer">Bantuan 24 Jam</a>
+                <a class="link link-footer">Qwords Rewards</a>
+                <a class="link link-footer">Knowledge Base</a>
+                <a class="link link-footer">Blog</a>
+                <a class="link link-footer">FAQ</a>
+                <a class="link link-footer">Kontak</a>
+            </nav>
+        </div>
+        <div class="flex flex-col gap-5">
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Mengenai Kami</h1>
+                <a class="link link-footer">Corporate Website</a>
+                <a class="link link-footer">Qwords History</a>
+                <a class="link link-footer">Qwords Differences</a>
+                <a class="link link-footer">Media Publication</a>
+                <a class="link link-footer">Network & Infrastructure</a>
+                <a class="link link-footer">Partner</a>
+                <a class="link link-footer">Career & Employment</a>
+                <a class="link link-footer">Mengapa Qwords Lebih Baik?</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">MSA/SLA/AUP</h1>
+                <a class="link link-footer">Master Service Agreement</a>
+                <a class="link link-footer">Refund Policy</a>
+                <a class="link link-footer">Privacy Policy</a>
+                <a class="link link-footer">Product Life Cycle Policy</a>
+                <a class="link link-footer">Penggunaan Merek dan Logo</a>
+                <a class="link link-footer">Migrasi Ke Qwords</a>
+            </nav>
+        </div>
+        <div class="flex flex-col gap-5">
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Layanan Lainnya</h1>
+                <a class="link link-footer">Extended Support</a>
+                <a class="link link-footer">Manage The Box Server</a>
+                <a class="link link-footer">Flexi Main Domain</a>
+                <a class="link link-footer">FTP Backup</a>
+                <a class="link link-footer">Offline Backup</a>
+                <a class="link link-footer">Hosting Insurance</a>
+                <a class="link link-footer">Software License</a>
+                <a class="link link-footer">Digital Signature</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Layanan Reseller</h1>
+                <a class="link link-footer">Afiliasi</a>
+                <a class="link link-footer">Qwords VIP Partner</a>
+                <a class="link link-footer">Private Label Hosting Reseller</a>
+                <a class="link link-footer">Private Label Domain Reseller</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Free Tools</h1>
+                <a class="link link-footer">Qwords Link-Q</a>
+                <a class="link link-footer">WhatsApp Link Generator</a>
+                <a class="link link-footer">Network Tools</a>
+                <a class="link link-footer">Saran Nama Domain</a>
+            </nav>
+        </div>
+        <div class="flex flex-col gap-5">
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Kantor Surabaya</h1>
+                <a class="link link-footer">Gedung Bursa Efek Indonesia Lt.10, Jl. Taman AIS Nasution No.21, Surabaya
+                    60271 - Indonesia 031 30008800</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Kantor Yogyakarta</h1>
+                <a class="link link-footer">Jl. Blotan no. 18 Kayen Wedomartani, Ngemplak, Sleman, Yogyakarta 55584 -
+                    Indonesia 0274 6058800</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Kantor Jakarta</h1>
+                <a class="link link-footer">Gedung Cyber 1 Lantai 3, Jl.Kuningan Barat no.8, Jakarta 12710 - Indonesia
+                    021 39708800</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100">Kantor Bandung</h1>
+                <a class="link link-footer">Jl. Terusan Setra Indah I No.19, Sukagalih, Sukajadi, Bandung 40163 -
+                    Indonesia 022 30508800</a>
+            </nav>
+            <nav class="flex flex-col">
+                <h1 class="footer-title opacity-100 mb-3">Ikuti Kami</h1>
+                <div class="flex gap-5">
+                    <a href="https://www.facebook.com/qwordsdotcom"><svg fill="#FFFFFF" height="50px" width="50px"
+                            version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512" xml:space="preserve"
+                            stroke="#FFFFFF">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M169.5,357.6l-2.9,38.3h-39.3 v133H77.7v-133H51.2v-38.3h26.5v-25.7c0-11.3,0.3-28.8,8.5-39.7c8.7-11.5,20.6-19.3,41.1-19.3c33.4,0,47.4,4.8,47.4,4.8l-6.6,39.2 c0,0-11-3.2-21.3-3.2c-10.3,0-19.5,3.7-19.5,14v29.9H169.5z">
+                                </path>
+                            </g>
+                        </svg></a>
+                    <a href="https://www.instagram.com/qwordsdotcom/"><svg fill="#FFFFFF" height="50px"
+                            width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512"
+                            xml:space="preserve" stroke="#FFFFFF">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g>
+                                    <path
+                                        d="M113,446c24.8,0,45.1-20.2,45.1-45.1c0-9.8-3.2-18.9-8.5-26.3c-8.2-11.3-21.5-18.8-36.5-18.8s-28.3,7.4-36.5,18.8 c-5.3,7.4-8.5,16.5-8.5,26.3C68,425.8,88.2,446,113,446z">
+                                    </path>
+                                    <polygon
+                                        points="211.4,345.9 211.4,308.1 211.4,302.5 205.8,302.5 168,302.6 168.2,346 ">
+                                    </polygon>
+                                    <path
+                                        d="M183,401c0,38.6-31.4,70-70,70c-38.6,0-70-31.4-70-70c0-9.3,1.9-18.2,5.2-26.3H10v104.8C10,493,21,504,34.5,504h157 c13.5,0,24.5-11,24.5-24.5V374.7h-38.2C181.2,382.8,183,391.7,183,401z">
+                                    </path>
+                                    <path
+                                        d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M241,374.7v104.8 c0,27.3-22.2,49.5-49.5,49.5h-157C7.2,529-15,506.8-15,479.5V374.7v-52.3c0-27.3,22.2-49.5,49.5-49.5h157 c27.3,0,49.5,22.2,49.5,49.5V374.7z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></a>
+                    <a href="https://twitter.com/qwordsdotcom"><svg fill="#FFFFFF" height="50px" width="50px"
+                            version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512"
+                            xml:space="preserve" stroke="#FFFFFF">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M215.2,361.2 c0.1,2.2,0.1,4.5,0.1,6.8c0,69.5-52.9,149.7-149.7,149.7c-29.7,0-57.4-8.7-80.6-23.6c4.1,0.5,8.3,0.7,12.6,0.7 c24.6,0,47.3-8.4,65.3-22.5c-23-0.4-42.5-15.6-49.1-36.5c3.2,0.6,6.5,0.9,9.9,0.9c4.8,0,9.5-0.6,13.9-1.9 C13.5,430-4.6,408.7-4.6,383.2v-0.6c7.1,3.9,15.2,6.3,23.8,6.6c-14.1-9.4-23.4-25.6-23.4-43.8c0-9.6,2.6-18.7,7.1-26.5 c26,31.9,64.7,52.8,108.4,55c-0.9-3.8-1.4-7.8-1.4-12c0-29,23.6-52.6,52.6-52.6c15.1,0,28.8,6.4,38.4,16.6 c12-2.4,23.2-6.7,33.4-12.8c-3.9,12.3-12.3,22.6-23.1,29.1c10.6-1.3,20.8-4.1,30.2-8.3C234.4,344.5,225.5,353.7,215.2,361.2z">
+                                </path>
+                            </g>
+                        </svg></a>
+                    <a href="https://www.youtube.com/user/qwordsvideo"><svg fill="#FFFFFF" height="50px"
+                            width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-143 145 512 512"
+                            xml:space="preserve" stroke="#FFFFFF">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g>
+                                    <polygon points="78.9,450.3 162.7,401.1 78.9,351.9 "></polygon>
+                                    <path
+                                        d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M241,446.8L241,446.8 c0,44.1-44.1,44.1-44.1,44.1H29.1c-44.1,0-44.1-44.1-44.1-44.1v-91.5c0-44.1,44.1-44.1,44.1-44.1h167.8c44.1,0,44.1,44.1,44.1,44.1 V446.8z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></a>
+                </div>
+            </nav>
+        </div>
+    </footer>
+
+    <footer class="footer bg-neutral text-white p-10">
+        <div class="flex flex-col w-full justify-center items-center gap-5">
+            <h1 class="text-lg font-bold text-center">Terdaftar di PSE, ICANN dan Partner Lainnya</h1>
+            <div class="flex justify-center items-center gap-2 mb-10">
+                <a href="https://pse.kominfo.go.id/tdpse-detail/1447"><img
+                        src="{{ asset('assets/footer/qwords-qr.webp') }}" width="100" height="100"
+                        alt="QR"></a>
+                <a
+                    href="https://www.icann.org/en/accredited-registrars?sort-direction=asc&sort-param=name&page=1&iana-number=4029&view-all=true"><img
+                        src="{{ asset('assets/footer/icann.webp') }}" width="100" height="100"
+                        alt="icann"></a>
+                <img src="{{ asset('assets/footer/litespeed.svg') }}" width="100" height="100"
+                    alt="Lite Speed">
+                <img src="{{ asset('assets/footer/whmadmin.svg') }}" width="100" height="100" alt="WHM Admin">
+                <img src="{{ asset('assets/footer/acronis-tech.webp') }}" width="100" height="100"
+                    alt="Acronis Tech">
+                <img src="{{ asset('assets/footer/acronis-sales.webp') }}" width="100" height="100"
+                    alt="Acronis Sales">
+            </div>
+            <h1 class="text-lg font-bold text-center">Copyright © 2024 PT. Qwords Company International</h1>
+        </div>
+    </footer>
+</body>
+
+</html>
